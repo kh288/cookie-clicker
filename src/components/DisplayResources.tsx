68bdcb591cf314e16e4569d709d298ea441235d3
Kevin Hernandez <kevin.hernandez288@gmail.com>
2023-12-19 13:24:54 -0800

    Added animation for cookies being updated

--- src/components/DisplayResources.tsx
+++ src/components/DisplayResources.tsx
@@ -1,23 +1,69 @@
+import { useEffect, useState } from "react";
+
 export default function DisplayResources(props: {
   money: number;
   cookies: number;
 }) {
+  const [moneyTrack, setMoneyTrack] = useState(props.money);
+  const [moneyChanged, setMoneyChanged] = useState(false);
+
+  const [cookiesTrack, setCookiesTrack] = useState(props.cookies);
+  const [cookiesChanged, setCookiesChanged] = useState(false);
+
+  useEffect(() => {
+    if (moneyTrack !== props.money) {
+      setMoneyChanged(true);
+    } else {
+      setMoneyChanged(false);
+    }
+    setTimeout(() => setMoneyChanged(false), 150); // Reset after animation
+    setMoneyTrack(props.money);
+  }, [props.money]);
+
+  useEffect(() => {
+    if (cookiesTrack !== props.cookies) {
+      setCookiesChanged(true);
+    } else {
+      setCookiesChanged(false);
+    }
+    setTimeout(() => setCookiesChanged(false), 150); // Reset after animation
+    setCookiesTrack(props.cookies);
+  }, [props.cookies]);
+
   return (
     <>
-      <div className="grid bg-slate-700 rounded p-3 inset-rim-light">
-        <code className="absolute">5🍪 = 1💵</code>
+      <div className="grid bg-slate-700 rounded p-2 inset-rim-light">
         <div className="flex justify-evenly">
           <div className="flex">
-            <h3 className="text-4xl">💵</h3>
-            <h3 className="text-4xl">{props.money.toLocaleString("en-US")}</h3>
+            <h3
+              className={`text-4xl pr-2 ${
+                moneyChanged ? "scale-110" : ""
+              } transition-transform`}>
+              💵
+            </h3>
+            <h3
+              className={`text-4xl ${
+                moneyChanged ? "scale-110" : ""
+              } transition-transform`}>
+              {props.money.toLocaleString("en-US")}
+            </h3>
           </div>
           <div className="flex">
-            <h3 className="text-4xl">🍪</h3>
-            <h3 className="text-4xl">
+            <h3
+              className={`text-4xl pr-2 ${
+                cookiesChanged ? "scale-110" : ""
+              } transition-transform`}>
+              🍪
+            </h3>
+            <h3
+              className={`text-4xl ${
+                cookiesChanged ? "scale-110" : ""
+              } transition-transform`}>
               {props.cookies.toLocaleString("en-US")}
             </h3>
           </div>
         </div>
+        <code>5🍪 = 1💵</code>
       </div>
     </>
   );
